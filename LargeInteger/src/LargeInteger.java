ad881b23a59e7a9763f0a4a6bfc93159d215f704
Sean Gibbons <gibbonss@bishopireton.org>
2017-09-12 13:54:57 -0400

    complete

--- LargeInteger/src/LargeInteger.java
+++ LargeInteger/src/LargeInteger.java
@@ -8,13 +8,12 @@ public class LargeInteger {
 	 * Represents the total length of all the arrays in order to allow basic
 	 * operations to be performed and have the digits line up correctly
 	 */
-	private static final int N = 30;
+	private static final int N = 10;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		test();
 	}
 
@@ -31,8 +30,8 @@ public class LargeInteger {
 	 */
 	private static int[] add(int[] a, int[] b) {
 		int[] result = new int[N];
-		for (int i = N; i > 0; i--) {
-			result[i] = (a[i] + b[i]) % 10;
+		for (int i = N - 1; i > 0; i--) {
+			result[i] = (a[i] + b[i] + result[i]) % 10;
 			result[i - 1] = (a[i] + b[i]) / 10;
 
 		}
@@ -44,40 +43,33 @@ public class LargeInteger {
 	 * tests the add method as described in Chapter 9, Exercise 25, p. 264
 	 * 
 	 */
-	/**
-	 * 
-	 */
 	public static void test() {
 		int[] a = new int[N];
 
 		for (int i = N - 1; i > 0; i--) {
-			a[i] = (int) Math.random() * 9 + 1;
+			a[i] = (int) (Math.random() * 9) + 1;
 		}
 		int[] b = new int[N];
 
 		for (int i = N - 1; i > 0; i--) {
-			a[i] = (int) Math.random() * 9 + 1;
+			b[i] = (int) (Math.random() * 9) + 1;
 
 		}
 		int[] sum = add(a, b);
 
 		System.out.print("The sum of ");
 		for (int i = 0; i < N; i++) {
-			if (a[i] != 0)
-				System.out.print(a[i]);
+			System.out.print(a[i]);
 		}
 		System.out.print(" and ");
 		for (int i = 0; i < N; i++) {
-			if (a[i] != 0)
-				System.out.print(b[i]);
+			System.out.print(b[i]);
 		}
-		
+
 		System.out.print(" is ");
-		
-		
+
 		for (int i = 0; i < N; i++) {
-			if (sum[i] != 0)
-				System.out.print(b[i]);
+			System.out.print(sum[i]);
 		}
 	}
 }
